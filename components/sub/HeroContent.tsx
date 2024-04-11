55f8a7398de584c8e73100e173ba2f41f347fdbc
vymn mohvmd <78211839+vymn@users.noreply.github.com>
2024-04-11 12:38:21 +0400

    typo HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -46,7 +46,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Software | CyperSecurity Engineer with extensive
+          I&apos;m a Software | CyberSecurity Engineer with extensive
           experience, working with Mobile development, Web development and
           utilizing Red Teaming tools.
         </motion.p>
